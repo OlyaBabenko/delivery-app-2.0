3c90c7240a842769ba68b2f41df6bcc725e7178e
Olya Babenko <babenko.olia@gmail.com>
2024-04-23 16:38:43 +0300

    update: add prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 3,
+    semi: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: true,
+ };
+ 
+ export default config;
