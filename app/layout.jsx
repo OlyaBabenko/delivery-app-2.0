cd9ffb3a672bd0b9af5c82a009ffbe2a69a5caab
Olya Babenko <babenko.olia@gmail.com>
2024-04-24 14:40:01 +0300

    clean up

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+   title: 'Yum yum delivery',
+};
+
+export default function RootLayout({ children }) {
+   return (
+      <html lang='en'>
+         <body className={inter.className}>
+            <div className='relative grid min-h-screen w-full grid-rows-[4rem_auto]'>
+               <Header />
+               <div className='bg-primary-100/70'>{children}</div>
+               {/* <Footer /> */}
+            </div>
+         </body>
+      </html>
+   );
+}
--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,41 +1,43 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import logo from "@/assets/image/Yum-yum-delivery-purple.png";
-import Link from "next/link";
-// import ModalAuthorization from "./ModalAuthorization/ModalAuthorization";
-import useCart from "@/store/cart";
-import useUser from "@/store/user";
-import Image from "next/image";
-import CartIcon from '@/assets/icon/cart.svg'
-import UserIcon from '@/assets/icon/user.svg'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import useCart from '@/store/cart';
+import useUser from '@/store/user';
+import logo from '@/assets/image/Yum-yum-delivery-purple.png';
+import CartIcon from '@/assets/icon/cart.svg';
+import UserIcon from '@/assets/icon/user.svg';
 
 const Header = () => {
-    const { userInfo } = useUser();
-    const { cart } = useCart();
-    const count = cart?.length;
-    const [modalActive, setModalActive] = useState(false)
+   const { userInfo } = useUser();
+   const { cart } = useCart();
+   const count = cart?.length;
 
-    return (
-        <div className='sticky top-0 left-0 pr-2 h-16 flex gap-2 items-center'>
-            <Link href="/" className='relative block w-60 h-full'>
-                <Image src={logo} alt={'logo'} fill={true} style={{ objectFit: "cover" }} />
-            </Link>
-            <Link href="/cart" className="block ml-auto p-2 rounded-full hover:bg-primary-100">
-                <CartIcon className='w-5 h-5 text-primary' />
-                {count > 0 && <div className=''>{count}</div>}
-            </Link>
-            {userInfo 
-                ? <Link href="/profile" className="block p-2 rounded-full hover:bg-primary-100">
-                    <UserIcon className='w-5 h-5 text-primary' />
-                  </Link> 
-                : <button className="p-2 rounded-full hover:bg-primary-100">
-                    <UserIcon className='w-5 h-5 text-primary' />
-                  </button>
-            }
-            {/* <ModalAuthorization setRegister={setIsRegisterUser} active={modalActive} setActive={setModalActive}/> */}
-        </div>
-    )
-}
+   return (
+      <div className='sticky left-0 top-0 flex h-16 items-center gap-2 pr-2'>
+         <Link href='/' className='relative block h-full w-60'>
+            <Image src={logo} alt={'logo'} fill={true} style={{ objectFit: 'cover' }} />
+         </Link>
+         <Link
+            href='/cart'
+            className='relative ml-auto block rounded-full p-2 hover:bg-primary-100'
+         >
+            <CartIcon className='h-6 w-6 text-primary' />
+            {count > 0 && (
+               <span className='absolute right-0 top-0 block h-4 w-4 rounded-full bg-primary-400 text-center text-xs text-white'>
+                  {count}
+               </span>
+            )}
+         </Link>
+         <Link
+            href={userInfo ? '/account' : '/sign-in'}
+            className='block rounded-full p-2 hover:bg-primary-100'
+         >
+            <UserIcon className='h-6 w-6 text-primary' />
+         </Link>
+      </div>
+   );
+};
 
 export default Header;
