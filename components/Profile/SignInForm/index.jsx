22fedaf876f08d7fd70888c90cc223600cc63a16
Olya Babenko <babenko.olia@gmail.com>
2024-05-09 15:25:25 +0300

    chore: refactor inputs & buttons

--- components/Profile/SignInForm/index.jsx
+++ components/Profile/SignInForm/index.jsx
@@ -1,31 +1,14 @@
+'use client';
+
+import UIButton from '@/components/UI/UIButton';
+import UIInput, { UIInputPassword } from '@/components/UI/UIInput';
+
 const SignInForm = () => {
    return (
-      <form action='' className='mt-6 flex w-full flex-col gap-4'>
-         <div className='flex flex-col'>
-            <label htmlFor='email' className='text-sm text-primary'>
-               Email
-            </label>
-            <input
-               type='text'
-               name=''
-               id='email'
-               className='rounded-md border border-primary-300 bg-primary-100/20 px-3 py-2 focus-visible:outline-none'
-            />
-         </div>
-         <div className='flex flex-col'>
-            <label htmlFor='password' className='text-sm text-primary'>
-               Password
-            </label>
-            <input
-               type='text'
-               name=''
-               id='password'
-               className='rounded-md border border-primary-300 bg-primary-100/20 px-3 py-2 focus-visible:outline-none'
-            />
-         </div>
-         <button className='mt-3 w-full rounded-md bg-primary px-3 py-2.5 text-center text-white hover:bg-primary-500 active:bg-primary-400'>
-            Sign in
-         </button>
+      <form action='' className='mt-6 flex w-full flex-col gap-10'>
+         <UIInput labelText='Email' name='email' />
+         <UIInputPassword labelText='Password' name='password' />
+         <UIButton>Sign in</UIButton>
       </form>
    );
 };
--- components/Profile/SignUpForm/index.jsx
+++ components/Profile/SignUpForm/index.jsx
@@ -1,53 +1,16 @@
+'use client';
+
+import UIButton from '@/components/UI/UIButton';
+import UIInput, { UIInputPassword } from '@/components/UI/UIInput';
+
 const SignUpForm = () => {
    return (
-      <form action='' className='mt-6 flex w-full flex-col gap-4'>
-         <div className='flex flex-col'>
-            <label htmlFor='name' className='text-sm text-primary'>
-               Name
-            </label>
-            <input
-               type='text'
-               name='name'
-               id='name'
-               className='rounded-md border border-primary-300 bg-primary-100/20 px-3 py-2 focus-visible:outline-none'
-            />
-         </div>
-         <div className='flex flex-col'>
-            <label htmlFor='email' className='text-sm text-primary'>
-               Email
-            </label>
-            <input
-               type='text'
-               name='email'
-               id='email'
-               className='rounded-md border border-primary-300 bg-primary-100/20 px-3 py-2 focus-visible:outline-none'
-            />
-         </div>
-         <div className='flex flex-col'>
-            <label htmlFor='password' className='text-sm text-primary'>
-               Password
-            </label>
-            <input
-               type='text'
-               name='passwordConfirm'
-               id='password'
-               className='rounded-md border border-primary-300 bg-primary-100/20 px-3 py-2 focus-visible:outline-none'
-            />
-         </div>
-         <div className='flex flex-col'>
-            <label htmlFor='passwordConfirm' className='text-sm text-primary'>
-               Confirm password
-            </label>
-            <input
-               type='text'
-               name='passwordConfirm'
-               id='passwordConfirm'
-               className='rounded-md border border-primary-300 bg-primary-100/20 px-3 py-2 focus-visible:outline-none'
-            />
-         </div>
-         <button className='mt-3 w-full rounded-md bg-primary px-3 py-2.5 text-center text-white hover:bg-primary-500 active:bg-primary-400'>
-            Sign up
-         </button>
+      <form action='' className='mt-6 flex w-full flex-col gap-10'>
+         <UIInput labelText='Name' name='name' />
+         <UIInput labelText='Email' name='email' />
+         <UIInputPassword labelText='Password' name='password' />
+         <UIInputPassword labelText='Confirm password' name='confirmPassword' />
+         <UIButton>Sign in</UIButton>
       </form>
    );
 };
