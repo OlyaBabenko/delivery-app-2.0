6a0c3c588974587180527b53a5b43ba6106107f3
Olya Babenko <babenko.olia@gmail.com>
2024-05-09 15:26:11 +0300

    clean up

--- components/UI/UIButton.jsx
+++ components/UI/UIButton.jsx
@@ -1,7 +1,7 @@
-const UIButton = ({children}) => {
+const UIButton = ({ children }) => {
    return (
       <button className='mt-3 w-full rounded-md bg-primary px-3 py-2.5 text-center text-white hover:bg-primary-500 active:bg-primary-400'>
-        {children}
+         {children}
       </button>
    );
 };
